ff6da75f26ee1ee3bda81873e8b3f7213f16d54c
samuko-things <samuel.c.agba@gmail.com>
2024-06-26 13:36:50 +0100

    new-code update

--- examples/read_rpy_data/read_rpy_data.ino
+++ examples/read_rpy_data/read_rpy_data.ino
@@ -1,70 +1,67 @@
+#include <Wire.h>
+#include "sic_mpu9250_i2c_lib.h"
 
-/*
- * Basic example code shows how to read orientation data from the sic_mpu9250 driver shield module
- * which have been succesfully calibrated with filter and covariances setup 
- * 
- * The code basically reads roll, pitch, and yaw values from the sic_mpu9250 connected to it.
- * read printed values from serial monitor or serial plotter.
- * 
- * you can copy the code and use it in your project as you will.
- */
+// Define Slave I2C Address
+const byte IMU_ADDRESS = 104; // 0x68
+SIC imu(IMU_ADDRESS);
 
-// Samuko Imu Compute (SIC) i2c communication library
-#include <sic_mpu9250_i2c_lib.h>
-
-// please update with the address with that which you set when doing 
-// calibration and filter setup with the sic_mpu9250_setup_application
-uint8_t imuAddress = 104; // i.e 0x68 in HEX
-SIC imu(imuAddress);
-
-float toRad = 2*PI/360;
-float toDeg = 1/toRad;
-
-float roll, pitch, yaw; // create variables to store orientations
-
-long prevSampleTime;
-long sampleTime = 20; // millisec
+float toRad = 2 * PI / 360;
+float toDeg = 1 / toRad;
 
 void setup()
 {
-  // start i2c communication
-  Wire.begin(); 
 
-  // setup serial communication to print result on serial minitor               
+  // Initialize I2C communications as Master
+  Wire.begin();
+
+  // Setup serial monitor
   Serial.begin(115200);
+  Serial.println("I2C Master Demonstration");
 
   // wait for the imu module to fully setup
-  for (int i=1; i<=10; i+=1){
-    delay(1000); 
+  for (int i = 1; i <= 10; i += 1)
+  {
+    delay(1000);
     Serial.println(i);
   }
-  
-  prevSampleTime = millis();
+
+  // // Send filter gain value of 0.1
+  // imu.setFilterGain(1.0);
+
+  // read the filter gain
+  float madgwickFilterGain;
+  imu.getFilterGain(madgwickFilterGain);
+
+  Serial.print("Madgwick Filter Gain: ");
+  Serial.println(madgwickFilterGain, 3);
+
+  // read the roll pitch and yaw value.
+  float roll_var, pitch_var, yaw_var;
+  imu.getRollVariance(roll_var);
+  imu.getPitchVariance(pitch_var);
+  imu.getYawVariance(yaw_var);
+
+  String msg = "RPY_variance: [" + String(roll_var, 10) + "," + String(pitch_var, 10) + "," + String(yaw_var, 10) + "]";
+  Serial.println(msg);
+  Serial.println();
+
+  delay(3000);
 }
 
-
- 
 void loop()
-{  
-  
-  if ((millis() - prevSampleTime) >= sampleTime) {
-    /* CODE SHOULD GO IN HERE*/
+{
 
-    imu.getRPY(roll, pitch, yaw); // read roll, pitch, yaw in radians
+  delay(50);
+  // read the roll pitch and yaw value in (rad/s).
+  float roll, pitch, yaw;
+  imu.getRPY(roll, pitch, yaw);
 
-    Serial.print(roll);
-    Serial.print(", ");
-    Serial.print(pitch);
-    Serial.print(", ");
-    Serial.println(yaw);
+  String msg = "RPY (rad/s): [" + String(roll, 4) + "," + String(pitch, 4) + "," + String(yaw, 4) + "]";
+  Serial.println(msg);
 
-    // Serial.print(roll*toDeg,1);
-    // Serial.print(", ");
-    // Serial.print(pitch*toDeg,1);
-    // Serial.print(", ");
-    // Serial.println(yaw*toDeg,1);
-    
-    prevSampleTime = millis();
-  }
-
+  // Serial.print(roll * toDeg);
+  // Serial.print(", ");
+  // Serial.print(pitch * toDeg);
+  // Serial.print(", ");
+  // Serial.println(yaw * toDeg);
 }
--- sic_mpu9250_i2c_lib.h
+++ sic_mpu9250_i2c_lib.h
@@ -2,44 +2,47 @@
 #define SIC_MPU9250_I2C_LIB_H
 
 #if ARDUINO >= 100
-  #include <Arduino.h>
+#include <Arduino.h>
 #else
-  #include <WProgram.h>
+#include <WProgram.h>
 #endif
-
 
 #include <Wire.h>
 #include <math.h>
 
-
-class SIC {
+class SIC
+{
 public:
-  SIC (int);
+  SIC(int);
 
-  void getRPY(float&, float&, float&);
-  void getRPYrate(float&, float&, float&);
-  void getAcc(float&, float&, float&);
-  void getQuat(float&, float&, float&, float&);
-  void getRPYvariance(float&, float&, float&);
-  void getRPYrateVariance(float&, float&, float&);
-  void getAccVariance(float&, float&, float&);
+  void setFilterGain(float);
+  void getFilterGain(float &);
 
-  
-private:
+  void getRPY(float &, float &, float &);
+  void getGyro(float &, float &, float &);
+  void getAcc(float &, float &, float &);
+  void getQuat(float &, float &, float &, float &);
+
+  void getRollVariance(float &);
+  void getPitchVariance(float &);
+  void getYawVariance(float &);
+
+  void getAxVariance(float &);
+  void getAyVariance(float &);
+  void getAzVariance(float &);
+
+  void getGxVariance(float &);
+  void getGyVariance(float &);
+  void getGzVariance(float &);
+
   int slaveAddr;
   String dataMsg = "", dataMsgBuffer = "", dataBuffer[3];
   float valA, valB, valC;
 
   void get(String);
-
+  void send(String, float);
   void masterSendData(String);
-
-  // String masterReceiveData();
-
-  String masterReceiveDataLarge();
-
-  // String masterReceiveDataChar();
-  
+  String masterReceiveData(byte);
 };
 
 #endif
